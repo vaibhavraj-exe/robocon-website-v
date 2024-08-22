973efa730deac469a5a64880be46ccc775c9c156
vaibhavraj-exe <vraj.exe@gmail.com>
2024-08-22 11:55:09 +0530

    feat: multisubmit prevention

--- src/app/orientation_reg/page.tsx
+++ src/app/orientation_reg/page.tsx
@@ -277,6 +277,7 @@ export default function App() {
                   type="submit"
                   className={`text-white ${loading ? "bg-gray-900" : "bg-blue-700 hover:bg-blue-800"} focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
                   value={loading ? "Loading..." : "Register"}
+                  disabled={loading}
                 />
                 <ReCAPTCHA
                   className=" flex justify-center items-center scale-75 flex-wrap"
