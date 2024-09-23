23f6d5c61caa47f9020ca6648c9ef36822ebd49b
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2024-09-23 14:35:00 +0000

    fix: responsiveness

--- src/components/AchievementsCard.tsx
+++ src/components/AchievementsCard.tsx
@@ -6,16 +6,16 @@ export default function Achievements(achievement: {
     description: string;
 }) {
     return (
-        <div className="flex items-center justify-center md:w-1/3 h-24 md:h-44 border-r-2 border-red pr-2 md:pr-5">
+        <div className="flex items-center w-full justify-center md:w-full lg:w-1/3 h-24 md:h-44 border-r-2 border-red pr-2 md:pr-5">
             <Image
                 src={achievement.imageSrc}
                 alt={achievement.altText}
-                width={500}
-                height={500}
+                width={100}
+                height={100}
                 className="w-auto h-full"
-                unoptimized
+                
             ></Image>
-            <div className="text-white text-right text-sm md:text-base pl-1">
+            <div className="text-white text-right text-sm md:text-base pl-1 text-ellipsis">
                 {achievement.description}
             </div>
         </div>
