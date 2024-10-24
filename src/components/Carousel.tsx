414e10f80a9d0bd1ec1ee1e7dd0431645fecd965
alvinbengeorge <alvinallen333@gmail.com>
2024-10-24 16:29:39 +0530

    feat: photo pop up

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 const events: Event[] = [
   {
     coverImage: "/events/ARC.JPG",
-    name: "ARC",
+    name: "Automation and Robotics Challenge(ARC) '24",
     description: `The National Level Automation & Robotics Challenge 2024, co-hosted by SRM Team Robocon and the Automation Club, gave students a platform to explore automation and robotics through exciting themes. The event concluded with an impressive final showdown on September 20th, highlighting innovative student projects.`,
     abstract: `The National Level Automation & Robotics Challenge 2024 was a remarkable initiative collaboratively organized by SRM Team Robocon and the Automation Club under the Department of Electronics & Instrumentation Engineering at SRM Institute of Science and Technology. This event, held from August 31st to September 20th, 2024, aimed to introduce school students to the dynamic fields of automation and robotics, promoting innovation in themes such as Automation in Daily Life, Industrial Automation, Environmental Robotics, and Robotics in Healthcare. Through two rigorous rounds — Prelims and the Showdown Round — the challenge fostered curiosity, technical skill development, and creative problem-solving, setting the stage for future advancements in robotics and automation among young minds.`,
     gallery: [
@@ -27,13 +27,13 @@ const events: Event[] = [
     description: `SolidWorks Bootcamp 2.0, organized by SRM Team Robocon, focused on enhancing CAD modeling skills over three days. Participants engaged in hands-on sessions and a competitive 3D modeling challenge. The workshop provided practical experience with SolidWorks, culminating in the grand prize—a Full-Year Subscription License for the best modeler.`,
     abstract: `SRM Team Robocon successfully organized SolidWorks Bootcamp 2.0, a 3-day intensive workshop held from February 26-28, 2024. Designed to equip participants with essential CAD modeling skills, the workshop featured expert-led, hands-on training sessions focused on SolidWorks, one of the most widely used CAD software in engineering and product design. The event culminated in a 3D modeling competition, allowing participants to apply their newly acquired skills in a real-world scenario. The top-performing participant was awarded a prestigious Full-Year Subscription License of SolidWorks. The workshop not only enhanced technical proficiency but also fostered collaboration and innovation, providing participants with an invaluable learning experience and a platform to showcase their creativity in CAD design.`,
     gallery: [
-      '/events/gallery/sw2/PXL_20240226_060420417.jpg', 
-      '/events/gallery/sw2/PXL_20240226_073410947.jpg', 
-      '/events/gallery/sw2/PXL_20240226_091814266.jpg', 
-      '/events/gallery/sw2/PXL_20240226_092040330.PORTRAIT.ORIGINAL.jpg', 
-      '/events/gallery/sw2/PXL_20240226_103224247.jpg', 
-      '/events/gallery/sw2/PXL_20240227_083032522.jpg', 
-      '/events/gallery/sw2/PXL_20240227_095017989.jpg', 
+      '/events/gallery/sw2/PXL_20240226_060420417.jpg',
+      '/events/gallery/sw2/PXL_20240226_073410947.jpg',
+      '/events/gallery/sw2/PXL_20240226_091814266.jpg',
+      '/events/gallery/sw2/PXL_20240226_092040330.PORTRAIT.ORIGINAL.jpg',
+      '/events/gallery/sw2/PXL_20240226_103224247.jpg',
+      '/events/gallery/sw2/PXL_20240227_083032522.jpg',
+      '/events/gallery/sw2/PXL_20240227_095017989.jpg',
       '/events/gallery/sw2/PXL_20240228_112637971 (1).jpg'
     ],
   },
@@ -43,19 +43,20 @@ const events: Event[] = [
     description: `The SolidWorks Bootcamp 2023 was a 3-day intensive workshop organized by SRM Team Robocon, designed to help students master the basics of CAD using SolidWorks. Through expert guidance from the team’s skilled designers, participants learned how to create precise 3D models and simulate designs. Limited seats ensured an interactive experience, culminating in a 3D modeling competition to celebrate exceptional work and creativity.`,
     abstract: `The SolidWorks Bootcamp 2023 marked the beginning of SRM Team Robocon's efforts to bring CAD design and simulation to students across various disciplines. This immersive 3-day workshop provided participants with the tools and knowledge to dive into SolidWorks, a powerful CAD software used by professionals worldwide. Through step-by-step guidance, students learned how to design with precision, bringing their ideas to life in 3D. The bootcamp concluded with an exciting competition, where participants showcased their new skills in 3D modelling, pushing the boundaries of creativity and precision. This workshop laid the foundation for students to explore the vast possibilities of CAD in the world of engineering and design.`,
     gallery: [
-      '/events/gallery/sw1/IMG-20230419-WA0029.jpg', 
-      '/events/gallery/sw1/IMG-20230419-WA0036.jpg', 
-      '/events/gallery/sw1/IMG-20230419-WA0038.jpg', 
-      '/events/gallery/sw1/IMG-20230424-WA0006.jpg', 
-      '/events/gallery/sw1/IMG-20230424-WA0018.jpg', 
-      '/events/gallery/sw1/IMG-20241022-WA0027.jpg', 
-      '/events/gallery/sw1/IMG-20241022-WA0028.jpg', 
-      '/events/gallery/sw1/IMG_20230424_191956_267.jpg', 
-      '/events/gallery/sw1/IMG_20230424_191956_474.jpg'],
+      '/events/gallery/sw1/IMG-20230419-WA0029.jpg',
+      '/events/gallery/sw1/IMG-20230419-WA0036.jpg',
+      '/events/gallery/sw1/IMG-20230419-WA0038.jpg',
+      '/events/gallery/sw1/IMG-20230424-WA0006.jpg',
+      '/events/gallery/sw1/IMG-20241022-WA0027.jpg',
+      '/events/gallery/sw1/IMG-20241022-WA0028.jpg',
+      '/events/gallery/sw1/IMG_20230424_191956_474.jpg',
+      '/events/gallery/sw1/IMG_20230424_191956_267.jpg',
+      '/events/gallery/sw1/IMG-20230424-WA0018.jpg',
+    ],
   },
   {
     coverImage: "/events/gallery/Altium/GKVK9246.JPG",
-    name: "Altium Event",
+    name: "Interview with Altium Team",
     description: `Rae Callender, VP of Altium Designer Education, visited SRMIST on September 4, 2024, to interact with student teams using Altium software in their robotics, electronics, and circuit design projects. Among the teams was SRM Team Robocon, represented by Mohammed Sadriwala, who discussed the importance of Altium in streamlining their design processes. The visit emphasized the software's impact on student projects and explored future collaborations between Altium and SRMIST.`,
     abstract: `On September 4, 2024, SRM Institute of Science and Technology hosted Rae Callender, Vice President of Altium Designer Education, for an engaging visit focused on student teams utilizing Altium Designer in their projects. Accompanied by Mr. Vikram Poojary and Mr. Madhesh Perumal, Rae interacted with four prominent student engineering teams, including SRM Team Robocon. The teams demonstrated how Altium Designer has been instrumental in improving their design precision and workflow efficiency. The visit highlighted the significant role Altium plays in shaping students’ engineering careers and laid the foundation for future collaborations between SRMIST and Altium.`,
     gallery: [
@@ -85,27 +86,29 @@ export default function Carousel() {
     console.log(isOpen);
   }, [isOpen]);
   return (
-    <div className=" w-3/4 h-full flex gap-2 overflow-y-scroll no-scrollbar">
+    <div className="w-full lg:w-5/6 h-full flex gap-2 overflow-y-scroll rounded-lg snap-mandatory snap-x carousel">
       {events.map((event: Event, index: number) => {
         return (
-          <button
-            key={index}
-            className="h-full"
-            onClick={() => {
-              open(isOpen ? null : event);
-            }}
-          >
-            <div
-              className="w-[350px] h-[500px] bg-cover bg-center rounded-lg hover:w-[450px] duration-500"
-              style={{ backgroundImage: `url(${event.coverImage})` }}
+          <>
+            <button
+              key={index}
+              className="h-full"
+              onClick={() => {
+                open(isOpen ? null : event);
+              }}
             >
-              <div className="bg-black opacity-0 hover:opacity-70 w-full h-full flex items-center justify-center rounded-lg duration-500">
-                <div>
-                  <h2 className="text-white text-xl">{event.name}</h2>
-                  {/* <p className="text-white text-sm">{event.abstract}</p> */}
+              <div
+                className="w-[350px] h-[500px] bg-cover bg-center rounded-lg  duration-500 carousel-element snap-center"
+                style={{ backgroundImage: `url(${event.coverImage})` }}
+              >
+                <div className="bg-black opacity-0 hover:opacity-70 w-full h-full flex items-center justify-center rounded-lg duration-500">
+                  <div>
+                    <h2 className="text-white text-xl">{event.name}</h2>
+                    {/* <p className="text-white text-sm">{event.abstract}</p> */}
+                  </div>
                 </div>
               </div>
-            </div>
+            </button>
             {isOpen && (
               <DialogBox
                 name={isOpen.name}
@@ -113,9 +116,11 @@ export default function Carousel() {
                 description={isOpen.description}
                 abstract={isOpen.abstract}
                 gallery={isOpen.gallery}
+                setOpen={open}
+                isOpen={isOpen !== null}
               />
             )}
-          </button>
+          </>
         );
       })}
     </div>
--- src/components/DialogBox.tsx
+++ src/components/DialogBox.tsx
@@ -1,5 +1,12 @@
 import { Event } from "@/constants/types";
 import Image from "next/image";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import AOS from "aos";
+import { useEffect, useState } from "react";
+
+const imgLoader = (src: string) => {
+  return src
+}
 
 export default function DialogBox({
   name,
@@ -7,40 +14,91 @@ export default function DialogBox({
   coverImage,
   abstract,
   gallery,
-}: Event) {
+  setOpen,
+  isOpen
+}: Event & { setOpen: (isOpen: Event | null) => void; isOpen: boolean }) {
+  const [isImageOpen, setImageOpen] = useState<string | null>(null);
+  useEffect(() => {
+    AOS.init({
+      duration: 150,
+      once: false,
+    });
+  }, []);
   return (
-    <div className="dialog-box text-white bg-black overflow-y-auto w-full h-full lg:w-3/4 border-y-0 border-x-0 lg:border-x-1 no-scrollbar grid gap-2">
-      <div className="text-center">
-        <h2 className="text-2xl p-2 font-bold underline">{name}</h2>
-      </div>
-      <div className="grid place-items-center gap-2 lg:flex">
-        <div className="grid place-items-center h-96">
-          <Image
-            src={coverImage}
-            alt={name}
-            width={400}
-            height={400}
-            className="rounded-xl h-96"
-          />
+    <div className="dialog-box text-white bg-black overflow-y-scroll w-full h-full grid place-items-center no-scrollbar">
+      <div className=" no-scrollbar grid gap-2 w-full h-full lg:w-5/6 py-2 px-4 border-y-0 rounded-3xl shadow-red shadow-2xl overflow-y-scroll">
+        <div className="grid place-items-end">
+          <button className="close-button bg-red p-2 rounded-lg"
+            onClick={() => {
+              setOpen(null);
+            }}
+          >
+            Close
+          </button>
         </div>
-        <div className="bg-red rounded-lg p-4 w-fit h-96 overflow-y-auto grid place-items-center"><p>{abstract}</p></div>
-      </div>
-      <div>
-        <p className="p-2">{description}</p>
-      </div>
-      <div className="grid place-items-center ">
-        <div className="grid place-items-center grid-cols-2 lg:grid-cols-3 gap-2 w-full">
-          {gallery.map((image, index) => (
-            <div key={index} className="bg-cover bg-center w-full h-72 lg:h-[500px] rounded-xl" style={{ backgroundImage: `url(${image})` }}>
+        <div className="text-center">
+          <h2 className="text-2xl p-2 font-bold underline">{name}</h2>
+        </div>
+        <div className="grid place-items-center gap-2 lg:flex">
+          <div className="grid place-items-center h-96">
+            <Image
+              src={coverImage}
+              alt={name}
+              width={400}
+              height={400}
+              className="rounded-xl h-96"
+            />
+          </div>
+          <div className="bg-red rounded-lg p-4 w-fit h-96 overflow-y-auto grid place-items-center">
+            <p className="text-justify">{abstract}</p>
+          </div>
+        </div>
+        <div>
+          <p className="p-2 text-justify">{description}</p>
+        </div>
+        <ResponsiveMasonry columnsCountBreakPoints={{ 400: 1, 500: 2, 600: 3 }} className="md:mx-10">
+          <Masonry gutter="7px">
+            {gallery.map((image, i) => (
+              <Image
+                key={i}
+                src={image}
+                style={{ width: "100%", display: "block" }}
+                alt=""
+                className="rounded-lg"
+                width={2400}
+                height={2400}
+                loader={() => imgLoader(image)}
+                onClick={() => {
+                  setImageOpen(image);
+                }}
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+        <div className="py-4 grid place-items-center">
+          <button className="close-button bg-red p-2 rounded-lg w-1/4"
+            onClick={() => {
+              setOpen(null);
+            }}
 
-            </div>
-          ))}
+          >
+            Close
+          </button>
         </div>
-      </div>
-      <div className="py-4">
-        <button className="close-button bg-red p-2 rounded-lg w-1/4">
-          Close
-        </button>
+        {isImageOpen && (
+          <div className="image-box grid bg-black w-full h-full lg:w-fit lg:h-fit place-items-center">
+            <div className="grid place-items-end w-full">
+              <button className="bg-red p-2 rounded-lg"
+                onClick={() => {
+                  setImageOpen(null);
+                }}
+              >
+                Close
+              </button>
+            </div>
+            <div className="bg-white p-4 h-screen overflow-auto"><Image src={isImageOpen} alt="" width={1200} height={1200} loader={() => imgLoader(isImageOpen)} className="rounded-lg" /></div>
+          </div>
+        )}
       </div>
     </div>
   );
