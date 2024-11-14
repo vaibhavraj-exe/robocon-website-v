0a3afeaadbf3ab5d09bbd0497a192a00c20f388e
MUKUL PARASHAR <mukulparashar0512@gmail.com>
2024-11-14 19:29:06 +0530

    project/projectcarousel achievement/achievementcarousel verticle in phone

--- src/components/AchievementsCarousel.tsx
+++ src/components/AchievementsCarousel.tsx
@@ -1,75 +1,75 @@
 "use client";
-import { Event } from "@/constants/types";
-import DialogBox from "./DialogBox";
+import { Achievement } from "@/constants/types";
+import DialogBox from "./DialogBox-achievements";
 import { useEffect, useState } from "react";
 
-export default function Carousel({ data }: { data: Event[] }) {
-  const [isOpen, open] = useState<Event | null>(null);
-  const [isPhone, setIsPhone] = useState<boolean>(window.innerWidth <= 768);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsPhone(window.innerWidth <= 768);
-    };
 
-    window.addEventListener("resize", handleResize);
+export default function Carousel({ data }: { data: Achievement[] }) {
+  const [isOpen, open] = useState<Achievement | null>(null);
+  const [isPhone, setIsPhone] = useState<boolean>(false);
+
     
-    // Cleanup the event listener on component unmount
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
   
   useEffect(() => {
     console.log(isOpen);
-  }, [isOpen]);
+  }, [isOpen]); 
 
+  useEffect(() => {
+    setIsPhone(window.innerWidth <= 768);
+    console.log(isPhone);
+  }, []);
   return (
-    <div
-      className={`w-full ${
-        isPhone ? "flex-col" : "flex-row lg:w-5/6"
-      } h-full flex gap-2 overflow-y-scroll rounded-lg snap-mandatory ${
-        isPhone ? "snap-y" : "snap-x"
-      } carousel`}
-    >
-      {data.map((event: Event, index: number) => (
-        <button
-          key={index}
-          className={`${
-            isPhone ? "w-full h-auto mb-4" : "h-full"
-          }`}
-          onClick={() => {
-            open(isOpen ? null : event);
-          }}
-        >
-          <div
-            className={`${
-              isPhone ? "w-full h-[400px]" : "w-[350px] h-[500px]"
-            } bg-cover bg-center rounded-lg duration-500 carousel-element ${
-              isPhone ? "snap-start" : "snap-center"
-            }`}
-            style={{ backgroundImage: `url(${event.coverImage})` }}
-          >
-            <div className="bg-black opacity-0 hover:opacity-70 w-full h-full flex items-center justify-center rounded-lg duration-500">
-              <div>
-                <h2 className="text-white text-xl">{event.name}</h2>
-                {/* <p className="text-white text-sm">{event.abstract}</p> */}
+    <div className="w-full lg:w-5/6 h-full grid lg:flex gap-2 overflow-x-scroll text-white rounded-lg snap-mandatory snap-x carousel place-items-center"> 
+      {data.map((achievements: Achievement, index: number) => {
+        return (
+          <div key={index}>
+            <button
+              key={index}
+              className="h-full"
+              onClick={() => {
+                open(isOpen ? null : achievements);
+              }}
+            >
+              <div
+                className="w-full  bg-cover bg-center rounded-lg  duration-500 carousel-element snap-center "
+                style={
+                  isPhone
+                    ? {
+                      backgroundImage: `url(${achievements.coverImage})`,
+                      width: `350px`,
+                      height: `${achievements.dimensions1.height / achievements.dimensions1.width * 350}px`,
+                    } : { backgroundImage: `url(${achievements.coverImage})`,
+                    height:`500px`,
+                    width:`${500*(achievements.dimensions1.width/achievements.dimensions1.height)}px`,
+                  }
+                }
+              >
+                <div className="bg-black opacity-0 hover:opacity-70 w-full h-full flex items-center justify-center rounded-lg duration-500">
+                  <div>
+                    <h2 className="text-white text-xl">{achievements.name}</h2>
+                    <p className="text-white text-sm">
+                    </p>
+                  </div>
+                </div>
               </div>
-            </div>
+            </button>
+            {isOpen && (
+              <DialogBox
+                name={isOpen.name}
+                coverImage={isOpen.coverImage}
+                description={isOpen.description}
+                abstract={isOpen.abstract}
+                gallery={isOpen.gallery}
+                setOpen={open}
+                dimensions1={isOpen.dimensions1}
+                isOpen={isOpen !== null}
+              />
+            )}
           </div>
-        </button>
-      ))}
-      {isOpen && (
-        <DialogBox
-          name={isOpen.name}
-          coverImage={isOpen.coverImage}
-          description={isOpen.description}
-          abstract={isOpen.abstract}
-          gallery={isOpen.gallery}
-          setOpen={open}
-          isOpen={isOpen !== null}
-        />
-      )}
+        );
+      })}
     </div>
+  
   );
 }
