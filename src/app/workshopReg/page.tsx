13c8617e078fa6e5320b65a8672c88d2dfbe6b36
Kshitij Mishra <kshitijmishra1302@gmail.com>
2025-02-11 17:45:23 +0530

    adds workshop registration page

--- src/app/workshopReg/page.tsx
+++ src/app/workshopReg/page.tsx
@@ -12,6 +12,7 @@ import { useRouter } from "next/navigation";
 export default function App() {
   const [captchaVerified, setCaptchaVerified] = useState(false);
   const router = useRouter();
+  const [hostel, setHostel] = useState("");
 
   const onChange = () => {
     setCaptchaVerified(true);
@@ -26,9 +27,9 @@ export default function App() {
     const officialEmail = formData.get("OfficialSRMEmailID");
     const contactNumber = formData.get("ContactNumber");
     const WhatsApp = formData.get("WhatsAppNumber");
-    const domain1 = formData.get("Domain1");
-    const domain2 = formData.get("Domain2");
-
+    const Hostel = formData.get("Hostel");
+    const Room = formData.get("Room");
+    const Workshop = formData.get("Workshop");
 
     // Email validation for @srmist.edu.in domain
     if (officialEmail) {
@@ -67,7 +68,7 @@ export default function App() {
     setMessage("");
     setError(false);
     fetch(
-      "https://script.google.com/macros/s/AKfycbyuoUw7O0eUfix3pZORKtVN5wNBIGHm6rWI4O6V1ANDJDaN6RojTSPWVeceWV7HDGCi/exec",
+      "https://script.google.com/macros/s/AKfycby9w-7ZDzsLxXakw5rlKGVjL_A3uZRbZDgvkfXukCPw06kpqn9pqD3DPMh3UuKOFfcFJg/exec",
       {
         method: "POST",
         body: formData,
@@ -78,7 +79,7 @@ export default function App() {
           // Registration successful
           setMessage("YOU ARE REGISTERED FOR THE EVENT");
           setTimeout(() => {
-            router.push("/orientation_reg/success");
+            router.push("/workshopReg/success");
           });
         } else {
           // Registration failed
@@ -107,17 +108,17 @@ export default function App() {
         <div className="text-xl m-5">
           Register Here!! <br />
         </div>
-        For Our Orientation.
+        For Our Workshops..
         <span className="text-lg text-red font-extrabold mt-5"></span>
       </h3>
       <div className=" flex p-10 gap-10 justify-center items-center flex-wrap">
-        <Image
+        {/* <Image
           className=" rounded-lg"
-          src="/events/orientation2024.png"
+          src="/events/payQR.jpg"
           width={340}
           height={340}
           alt=""
-        />
+        /> */}
         <div className="">
           <div className=" justify-center">
             <form className="max-w-md mx-auto" onSubmit={(e) => Submit(e)}>
@@ -141,8 +142,8 @@ export default function App() {
                   id="Full Registration Number"
                   className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
-                  pattern="^(RA23|RA24)[A-Za-z0-9]{11}$"
-  title="The registration number must start with RA23 or RA24 and be 15 characters long."
+                  
+                  title="The registration number must start with RA23 or RA24 and be 15 characters long."
                   required
                 />
                 <label className="peer-focus:font-medium absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
@@ -221,52 +222,83 @@ export default function App() {
                 </div>
               </div>
 
-              <div className="flex gap-7 pb-3 w-fit">
-                <div>
+              <div className="grid md:grid-cols-2 md:gap-6">
+
+              <div>
                   <label
-                    htmlFor="Domain"
+                    htmlFor="Hostel"
                     className="block mb-2 text-sm font-medium text-white whitespace-nowrap"
                   >
-                    1st Domain choice
+                    Select Hostel/Day-Scholar
                   </label>
                   <select
                     required
-                    name="Domain1"
-                    id="Domain"
-                    className="bg-gray-50 border border-gray-300 text-sm rounded-lg p-1 focus:ring-blue-500 focus:border-blue-500 block   placeholder-gray-400 text-black "
+                    name="Hostel"
+                    id="Hostel"
+                    defaultValue="default" 
+                    onChange={(e) => setHostel(e.target.value)}
+                    className="bg-gray-50 border border-gray-300 text-sm rounded-sm p-1 focus:ring-blue-500 focus:border-blue-500 block   placeholder-gray-400 text-black "
                   >
-                    <option disabled selected value="">
+                    <option disabled value="default">
                       select any one
                     </option>
-                    <option value="Web Development">Web Development</option>
-                    <option value="MCSOD-Corporate">MCSOCD-Creatives</option>
-                    <option value="IESED-Electronics">SIESED-Electronics</option>
-                    <option value="SPACED-Coding">SPACED-Coding</option>
-                    <option value="SAMBED-Mechanical">SAMBED-Mechanical</option>
+                    <option value="Day Scholar">Day Scholar</option>
+                    <option value="Kaari">Kaari</option>
+                    <option value="Paari">Paari</option>
+                    <option value="Oori">Oori</option>
+                    <option value="Adhyaman">Adhyaman</option>
+                    <option value="Nelson Mandela">Nelson Mandela</option>
+                    <option value="Sannasi A">Sannasi A</option>
+                    <option value="Sannasi C">Sannasi C</option>
+                    <option value="Agasthyar">Agasthyar</option>
+                    <option value="Malligai">Malligai</option>
+                    <option value="Mullai">Mullai</option>
+                    <option value="Manoranjitham">Manoranjitham</option>
+                    <option value="Thamarai">Thamarai</option>
+                    <option value="KC">KC</option>
+                    <option value="EsQ">EsQ</option>
+                    <option value="M Block">M Block</option>
+                    <option value="Meenakshi">Meenakshi</option>
+                    <option value="Began">Began</option>
                   </select>
                 </div>
+                
+                {!(hostel==="" || hostel === "Day Scholar") && <div className="relative z-0 w-full mb-5 group">
+                  <input
+                    type="tel"
+                    name="Room"
+                    id="Room"
+                    className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    placeholder=" "
+                  />
+                  <label className="peer-focus:font-medium absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                    Hostel Room Number
+                  </label>
+                </div>}
 
+      
+              </div>
+
+              <div className="flex gap-7 pb-3 pt-4 w-fit">
                 <div>
                   <label
-                    htmlFor="Domain"
+                    htmlFor="Workshop"
                     className="block mb-2 text-sm font-medium text-white whitespace-nowrap"
                   >
-                    2nd Domain choice
+                    Select Workshop
                   </label>
                   <select
-                  required
-                    name="Domain2"
-                    id="Domain"
-                    className="bg-gray-50 border border-gray-300 p-1 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block   placeholder-gray-400 text-black "
+                    required
+                    name="Workshop"
+                    id="Workshop"
+                    defaultValue="default" 
+                    className="bg-gray-50 border border-gray-300 p-1 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block   placeholder-gray-400 text-black "
                   >
-                    <option disabled selected value="">
+                    <option disabled value="default">
                       select any one
                     </option>
-                    <option value="Web Development">Web Development</option>
-                    <option value="MCSOD-Corporate">MCSOCD-Creatives</option>
-                    <option value="SIESED-Electronics">SIESED-Electronics</option>
-                    <option value="SPACED-Coding">SPACED-Coding</option>
-                    <option value="SAMBED-Mechanical">SAMBED-Mechanical</option>
+                    <option value="Altium">Altium</option>
+                    <option value="Solidworks">Solidworks</option>
                   </select>
                 </div>
               </div>
@@ -275,7 +307,9 @@ export default function App() {
                 <input
                   name="Name"
                   type="submit"
-                  className={`text-white ${loading ? "bg-gray-900" : "bg-blue-700 hover:bg-blue-800"} focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
+                  className={`text-white ${
+                    loading ? "bg-gray-900" : "bg-blue-700 hover:bg-blue-800"
+                  } focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
                   value={loading ? "Loading..." : "Register"}
                   disabled={loading}
                 />
